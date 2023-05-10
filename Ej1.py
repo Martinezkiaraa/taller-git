70ece7d0d604db03b115773d3440520e77f37324
Kiara Martinez <45863640@utdt.local>
2023-05-10 18:48:55 -0300

    ultima versión

--- Ej1.py
+++ Ej1.py
@@ -1,4 +1,11 @@
 def hola_mundo(n:str) -> str:
     print('Hola mundo' + n)
 def suma_y_resta(n:int, m:int) -> int:
+    if n == 3:   
+        x:bool = True
+    else:
+        x:bool = False
+    print(x)
+        
+        
     return n - m + n
